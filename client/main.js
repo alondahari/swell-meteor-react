bbdd8c4bb9497c835f1c8fbf0088c53eecacc0ff
jazzdragon <climbinghobo@gmail.com>
2016-12-27 19:27:58 -0700

    mounted

--- client/main.js
+++ client/main.js
@@ -1,7 +1,1 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from '/imports/ui/layouts/main/App'
-
-Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'));
-});
+import '/imports/startup/client'
